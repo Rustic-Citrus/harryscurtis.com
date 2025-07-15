e760ac258f063125ac89ae068797b0f430a4086f
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-07-15 11:37:47 +0100

    feat: implemented basic root page content

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Hello, World!</h1>
+    <div class="border-dashed border-black border-2">
+      <h1 class="text-2xl">Home</h1>
+      <p>This is the body of the Home component.</p>
+    </div>
   );
 }
