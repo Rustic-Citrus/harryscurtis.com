8546b74f1dd029c558ecc7b627def08a5149342b
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-07-17 22:50:24 +0100

    fix: upgraded to tailwind 4

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "../components/Navbar.tsx";
 
 export default function App({ Component }: PageProps) {
   return (
-    <html data-theme="lofi">
+    <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
