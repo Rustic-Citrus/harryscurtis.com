71c839d6799a63a44e1506bc1a075d9cb8591874
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-07-25 08:47:15 +0100

    feat: added a contact form success send page

--- routes/contact/success/index.tsx
+++ routes/contact/success/index.tsx
@@ -6,10 +6,55 @@ export default function ContactSuccess() {
       <Head>
         <title>Success!</title>
       </Head>
-      <div>
-        <h1>Success!</h1>
-        <p>Harry was sent your message.</p>
-        <p>You may now navigate away from this page.</p>
+      <div class="py-8">
+        <div class="card w-96 bg-base-100 card-lg shadow-sm max-h-1/2">
+          <div class="badge badge-success absolute -right-2 -top-2">
+            <svg
+              class="size-[1em]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                fill="currentColor"
+                stroke-linejoin="miter"
+                stroke-linecap="butt"
+              >
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-miterlimit="10"
+                  stroke-width="2"
+                >
+                </circle>
+                <polyline
+                  points="7 13 10 16 17 8"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="square"
+                  stroke-miterlimit="10"
+                  stroke-width="2"
+                >
+                </polyline>
+              </g>
+            </svg>
+            Success
+          </div>
+          <div class="card-body">
+            <h2 class="card-title">Message Sent</h2>
+            <p>
+              It looks like your message was successfully sent to my inbox. I normally reply within 48 hours. Thanks for reaching out!
+            </p>
+            <div class="justify-end card-actions">
+              <a class="btn btn-primary" href="/">
+                Home
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
