4c98deffc48e0eccac029a3adbc7b331c098390b
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-05 10:38:23 +0100

    refactor: general layout improvements

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,19 +4,22 @@ import posts from "../dummy/posts.json" with { type: "json" };
 export default function Home() {
   return (
     <div class="flex flex-col lg:flex-row justify-center gap-4">
-      <div class="flex flex-col gap-2 justify-center m-2">
-        <h1 class="text-3xl">
+      <div class="flex flex-col gap-2 justify-left lg:max-w-1/2 m-5 text-primary-content">
+        <h1 class="text-3xl pt-10">
           Learning in public
         </h1>
         <h2 class="text-xl">Harry Curtis</h2>
-        <p class="px-10">
+        <p class="text-lg">
           Welcome to a place where I share my experiences of learning about
           stuff that I find interesting, and things that I've made which I
           usually end up looking back on with horror.
         </p>
-        <small>PS The "S" is for "Stuart".</small>
+        <p>
+          You can also send me a message through the{" "}
+          <a href="/contact" class="text-primary">contact page</a>.
+        </p>
       </div>
-      <div class="flex flex-col justify-center m-2 max-w-full xl:max-w-1/2">
+      <div class="flex flex-col justify-center m-2">
         <h1 class="text-2xl">Featured notes</h1>
         <FeaturedPosts item={posts.slice(0, 3)} />
       </div>
