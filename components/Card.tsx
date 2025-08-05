0d353568b6157167b40914a9ed31b33c87209029
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-05 10:33:51 +0100

    refactor: improved reusability of card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,21 +1,20 @@
 import { CardProps } from "../types/index.ts";
-
 export const Card = ({ item }: CardProps) => {
   return (
-    <div class="card w-72 card-xl shadow-lg">
+    <div class="card card-xl shadow-lg">
       <div class="card-body">
         <h2 class="card-title">{item.title ?? ""}</h2>
         <p>
           {item.summary ?? ""}
         </p>
         <div class="justify-end card-actions">
-          <button
+          <a
             type="button"
-            class="btn btn-primary"
+            class={`btn ${item.buttonClass ?? "btn-primary"}`}
             href={item.link ?? ""}
           >
-            Read
-          </button>
+            {item.buttonText ?? "Read"}
+          </a>
         </div>
       </div>
     </div>
