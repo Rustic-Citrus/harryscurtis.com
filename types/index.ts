b0089eda348bc2a4600a6505ddd6b213904278b8
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-05 10:37:12 +0100

    refactor: added and edited types

--- types/index.ts
+++ types/index.ts
@@ -1,23 +1,28 @@
+import { createElement } from "preact/src/index.d.ts";
 import { type ComponentProps } from "preact";
-
 export interface Post {
-    title: string;
-    date_published: string;
-    summary: string;
-    content: string;
-    link: string;
+  title: string;
+  date_published: string;
+  summary: string;
+  content: string;
+  link: string;
 }
-
 export interface FeaturedPostsProps extends ComponentProps<"div"> {
-    item: Post[]
+  item: Post[];
 }
-
 export interface CardData {
-    title?: string;
-    summary?: string;
-    link?: string;
+  title?: string;
+  summary?: string;
+  link?: string;
+  buttonText?: string;
+  buttonClass?: string;
 }
-
 export interface CardProps extends ComponentProps<"div"> {
-    item: CardData;
+  item: CardData;
+}
+export interface InformationCardProps extends ComponentProps<"div"> {
+  type: "success" | "error";
+  title: string;
+  body: string | string[];
+  buttons?: createElement.JSX.Element | createElement.JSX.Element[];
 }
