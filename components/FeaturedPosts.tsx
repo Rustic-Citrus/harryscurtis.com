0cb439ad8c475a45d981f21a87ced56ad1f3134a
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-05 10:35:51 +0100

    refactor: adjusted layout from flex to grid

--- components/FeaturedPosts.tsx
+++ components/FeaturedPosts.tsx
@@ -1,9 +1,8 @@
 import { FeaturedPostsProps, Post } from "../types/index.ts";
 import { Card } from "./Card.tsx";
-
 export const FeaturedPosts = ({ item }: FeaturedPostsProps) => {
   return (
-    <div class="flex flex-wrap gap-2">
+    <div class="grid grid-cols-1 grid-flow-row gap-2 sm:grid-cols-2">
       {item.map((post: Post, _index) => {
         const cardData =
           (({ title, summary, link }) => ({ title, summary, link }))(post);
@@ -13,23 +12,15 @@ export const FeaturedPosts = ({ item }: FeaturedPostsProps) => {
           />
         );
       })}
-      <div class="stack stack-end w-72">
-        <div class="card w-72 card-xl shadow-lg">
-          <div class="card-body">
-            <h2 class="card-title">More notes</h2>
-            <p class="text-left">Click the button below to see all notes.</p>
-            <button type="button" class="btn btn-outline w-1/2" href="/posts">
-              See More
-            </button>
-          </div>
-        </div>
-        <div class="card w-72 card-xl shadow-lg">
-          <div class="card-body"></div>
-        </div>
-        <div class="card w-72 card-xl shadow-lg">
-          <div class="card-body"></div>
-        </div>
-      </div>
+      <Card
+        item={{
+          title: "More notes",
+          summary: "Click the button below to see all notes.",
+          link: "/posts",
+          buttonClass: "btn-outline",
+          buttonText: "See more",
+        }}
+      />
     </div>
   );
 };
