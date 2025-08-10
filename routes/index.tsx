a6a8103d595485443641fe1284f453b8fd4dc3ca
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-10 14:40:46 +0100

    fix: styling and made css classes DRYer

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,8 +3,8 @@ import posts from "../dummy/posts.json" with { type: "json" };
 
 export default function Home() {
   return (
-    <div class="flex flex-col lg:flex-row justify-center gap-4">
-      <div class="flex flex-col gap-2 justify-left lg:max-w-1/2 m-5 text-primary-content">
+    <div class="flex flex-col lg:flex-row justify-center gap-4 *:m-5 *:flex *:flex-col *:gap-2">
+      <div class="justify-left lg:max-w-1/2 text-primary-content">
         <h1 class="text-4xl pt-10 pb-4">
           Learning in public
         </h1>
@@ -16,11 +16,11 @@ export default function Home() {
         </p>
         <p>
           You can also send me a message through the{" "}
-          <a href="/contact" class="text-primary">contact page</a>.
+          <a href="/contact" class="text-secondary-content">contact page</a>.
         </p>
       </div>
-      <div class="flex flex-col justify-center m-2">
-        <h1 class="text-2xl">Featured notes</h1>
+      <div class="justify-center">
+        <h1 class="text-3xl pb-5">Featured notes</h1>
         <FeaturedPosts item={posts.slice(0, 3)} />
       </div>
     </div>
