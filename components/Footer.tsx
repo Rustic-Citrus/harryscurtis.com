7870bb6ba6c9d0f1f3d6da78da3f5a10940d3ae9
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-10 14:41:44 +0100

    style: changed footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer class="footer sm:footer-horizontal footer-center bg-base-100/25 p-4">
+    <footer class="footer sm:footer-horizontal footer-center bg-base-100 shadow-sm p-4">
       <aside>
         <p>
           Copyright © {year} - All rights reserved by Harry Stuart Curtis.
