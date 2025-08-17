77222529ca961f051c2336e1e5cb60b9ebb00789
Rustic-Citrus <mrcurtis.english@gmail.com>
2025-08-17 14:53:28 +0100

    feat: began integrating posts into notes

--- routes/notes/index.tsx
+++ routes/notes/index.tsx
@@ -1,7 +1,40 @@
+import data from "../../dummy/posts.json" with { type: "json" };
+import { Card } from "../../components/Card.tsx";
 export default function Notes() {
-    return (
-        <div>
-            <h1 class="text-3xl">This is a test.</h1>
-        </div>
-    )
-} 
+  return (
+    <div class="flex flex-col gap-2">
+      <h1 class="pt-4 text-3xl">Notes</h1>
+      <label class="input">
+        <svg
+          class="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input type="search" class="grow" placeholder="Search" />
+      </label>
+      <div class="grid col-span-3">
+        {data.map((post, _index) => {
+          const cardData =
+            (({ title, summary, link }) => ({ title, summary, link }))(post);
+          return (
+            <Card
+              item={cardData}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
