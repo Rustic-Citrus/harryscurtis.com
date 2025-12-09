71a89fa0ff3294e8d786bafe62a55ed19f34a558
Harry <harrycurtis@live.com>
2025-12-09 13:55:16 +0000

    refactor: updated text on homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -15,27 +15,73 @@ export default define.page(function Home(ctx) {
           <h1 class="text-4xl pt-10 font-bold pb-4">
             Harry Stuart Curtis
           </h1>
-          <h2 class="text-2xl italic">
-            Digital Consultant
-          </h2>{" "}
-          <p class="text-lg">
-            Based in the Hay-on-Wye area, I provide consulting and support
-            services for problems generally classified as "digital", such as IT,
-            Software Engineering and Data Analysis issues.
-          </p>
-          <p class="text-lg">
-            With experience and professional qualifications in Software
-            Engineering, Data Analysis and multiple programming languages, I am
-            equipped to handle the vast majority of problems that individuals
-            and businesses come across in their day-to-day operations.
-            Additionally, with 7 years of experience as a teacher, I'm also able
-            to provide assistance in IT training.
-          </p>
-          <p>
-            If you or your business would benefit from any of these services,
-            you can give me a call on 07379 555795. You can also send me a
-            message through the <a href="/contact">contact page</a>.
-          </p>
+          <span class="text-rotate text-7xl">
+            <span class="justify-items-left">
+              <span class="text-2xl">Digital Consultant</span>
+              <span class="text-2xl">Software Engineer</span>
+              <span class="text-2xl">Data Analyst</span>
+            </span>
+          </span>
+          <div class="collapse collapse-plus bg-base-100 border border-base-300">
+            <input type="radio" name="homepage-accordion" />
+            <div class="collapse-title font-semibold">
+              Which services do you offer?
+            </div>
+            <div class="collapse-content">
+              <p class="text-sm mb-4">
+                I offer a range of computer hardware and software services,
+                including software engineering, data analysis and basic IT
+                support. For more information, click the Services button below.
+              </p>
+              <a
+                href="/services"
+                class="flex justify-end hover:cursor-pointer"
+              >
+                <button
+                  class="btn btn-primary"
+                  type="button"
+                >
+                  Services
+                </button>
+              </a>
+            </div>
+          </div>
+          <div class="collapse collapse-plus bg-base-100 border border-base-300 mt-2">
+            <input type="radio" name="homepage-accordion" checked="checked" />
+            <div class="collapse-title font-semibold">
+              Where do you provide services?
+            </div>
+            <div class="collapse-content text-sm">
+              Based out of Hay-on-Wye, I can provide face-to-face services to
+              clients in Powys, Herefordshire and Monmouthshire. For
+              consultation services, I can also provide advice over the phone or
+              video call.
+            </div>
+          </div>
+          <div class="collapse collapse-plus bg-base-100 border border-base-300">
+            <input type="radio" name="homepage-accordion" />
+            <div class="collapse-title font-semibold">
+              How can I contact you?
+            </div>
+            <div class="collapse-content">
+              <p class="text-sm mb-4">
+                To arrange a consultation or to enquire about my services,
+                please contact me using the contact form, accessible through the
+                button below.
+              </p>
+              <a
+                href="/contact"
+                class="flex justify-end hover:cursor-pointer"
+              >
+                <button
+                  class="btn btn-primary"
+                  type="button"
+                >
+                  Contact Form
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
         <div class="justify-center">
           <h1 class="text-3xl font-bold pb-5">Featured Posts</h1>
