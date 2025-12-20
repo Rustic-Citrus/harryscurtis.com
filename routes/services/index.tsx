e5d56e44d681358c9ed94cf3d742a948500068d5
Harry <harrycurtis@live.com>
2025-12-20 17:52:26 +0000

    updated the styling of the cards on the services page

--- routes/services/index.tsx
+++ routes/services/index.tsx
@@ -2,60 +2,70 @@ import { define } from "../../utils.ts";
 
 export default define.page(() => {
   return (
-    <div class="flex flex-col justify-center items-center min-h-screen gap-4 mb-12 mt-6 lg:py-12 lg:gap-24">
-      <div class="card flex-col sm:flex-row w-5/6 sm:w-full bg-base-200 border border-base-content/50 transition-colors shadow-md mx-4 p-4 sm:mx-0 sm:max-w-3xl sm:min-h-xl">
-        <figure class="max-h-1/3 sm:max-h-full sm:w-1/2">
+    <div class="flex flex-col lg:flex-row justify-center items-center gap-4 px-6">
+      <div class="card flex-col w-5/6 bg-base-200 border border-base-content/50 transition-colors shadow-md mx-4 lg:w-full lg:mx-0 lg:max-w-3/4 lg:min-h-xl">
+        <figure class="max-h-64">
           <img
             src="mohammad-rahmani-oXlXu2qukGE-unsplash.jpg"
-            class="w-full bg-cover m-2 shadow-lg opacity-50"
+            class="object-none shadow-lg opacity-50"
             alt="Photo by Mohammad Rahmani on Unsplash"
             title="Photo by Mohammad Rahmani on Unsplash"
           />
         </figure>
-        <div class="card-body w-full sm:w-1/2">
-          <h2 class="card-title text-3xl">Software Engineering</h2>
-          <p class="py-2 sm:py-6 text-lg">
-            Includes designing, developing, and testing applications for
-            businesses and private individuals.
+        <div class="card-body">
+          <h2 class="card-title text-3xl">Software Engineering Projects</h2>
+          <p class="py-2 text-lg">
+            Software engineering means the design and development of
+            applications. It necessarily involves a substantial amount of
+            communication with the client to understand their needs, followed by
+            the planning, and execution of the project. Requisitioning a
+            software developer is most useful when you can't find the software
+            to meet your specific requirements.
           </p>
-          <ul>
-            <li>A lesson-planning application for English teachers.</li>
+          <ul class="list-inside list-disc">
+            <li>
+              An inventory database and management application for a second-hand
+              bookshop.
+            </li>
+            <li>A bespoke online forum for a community.</li>
+            <li>A website with an online shop for a clothes retailer.</li>
           </ul>
           <div class="card-actions justify-end">
           </div>
         </div>
       </div>
-      <div class="card flex-col-reverse sm:flex-row w-5/6 sm:w-full glass shadow-md mx-2 sm:mx-0 sm:max-w-3xl sm:min-h-xl">
-        <div class="card-body w-full sm:w-1/2">
-          <h2 class="card-title text-3xl">Digital Project Management</h2>
-          <p class="py-2 sm:py-6">
-            Includes creating and designing IT projects, such as finding the
-            right hardware and software products for a specific use case.
-          </p>
-          <div class="card-actions justify-end">
-          </div>
-        </div>
-        <figure class="max-h-1/3 sm:max-h-full sm:w-1/2">
+      <div class="card flex-col h-full w-5/6 bg-base-200 border border-base-content/50 transition-colors shadow-md mx-4 lg:w-full lg:mx-0 lg:max-w-3/4 lg:min-h-xl">
+        <figure class="max-h-64">
           <img
             src="daria-nepriakhina-zoCDWPuiRuA-unsplash.jpg"
-            class="w-full mask mask-squircle m-2 shadow-lg opacity-75 max-h-5/6"
+            class="object-none shadow-lg opacity-50"
             alt="Photo by Daria Nepriakhina on Unsplash"
             title="Photo by Daria Nepriakhina on Unsplash"
           />
         </figure>
+        <div class="card-body">
+          <h2 class="card-title text-3xl">Digital Transformation Projects</h2>
+          <p class="py-2 text-lg">
+            Digital transformation is the process of converting analogue into
+            digital systems. Often, this involves making procedural tasks
+            automated, saving you and your business time.
+          </p>
+          <div class="card-actions justify-end">
+          </div>
+        </div>
       </div>
-      <div class="card flex-col sm:flex-row w-5/6 sm:w-full glass shadow-md mx-2 sm:mx-0 sm:max-w-3xl sm:min-h-xl">
-        <figure class="max-h-1/3 sm:max-h-full sm:w-1/2">
+      <div class="card flex-col h-full w-5/6 bg-base-200 border border-base-content/50 transition-colors shadow-md mx-4 lg:w-full lg:mx-0 lg:max-w-3/4 lg:min-h-xl">
+        <figure>
           <img
             src="freestocks-I_pOqP6kCOI-unsplash.jpg"
-            class="w-full mask mask-squircle m-2 shadow-lg opacity-75 max-h-5/6"
+            class="shadow-lg opacity-50"
             alt="Photo by freestocks on Unsplash"
             title="Photo by freestocks on Unsplash"
           />
         </figure>
-        <div class="card-body w-full sm:w-1/2">
+        <div class="card-body">
           <h2 class="card-title text-3xl">IT Support & Diagnostics</h2>
-          <p class="py-2 sm:py-6">
+          <p class="py-2 text-lg">
             Includes fixing malfunctioning computer hardware and software.
           </p>
           <div class="card-actions justify-end">
