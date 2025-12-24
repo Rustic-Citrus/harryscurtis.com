2f8ecd382187c9705d82df0ae2ee78193a589609
Harry <harrycurtis@live.com>
2025-12-24 14:38:34 +0000

    updated styles on services page

--- components/ServicesCard.tsx
+++ components/ServicesCard.tsx
@@ -1,29 +1,25 @@
 interface ServicesCardProps {
-  imageUrl: string;
-  imageAlt: string;
-  imageTitle: string;
   cardTitle: string;
   cardBodyText: string;
 }
 
 export default function ServicesCard(
-  { imageUrl, imageAlt, imageTitle, cardTitle, cardBodyText }:
-    ServicesCardProps,
+  { cardTitle, cardBodyText }: ServicesCardProps,
 ) {
   return (
-    <div class="card flex-col flex-1 max-w-xl image-full border border-base-content/50 transition-colors shadow-lg">
-      <figure>
-        <img
-          src={imageUrl}
-          alt={imageAlt}
-          title={imageTitle}
-        />
-      </figure>
-      <div class="card-body text-blue-50">
+    <div class="card card-xl max-w-xl bg-base-100 border-2 border-base-content/50 transition-colors shadow-lg">
+      <div class="card-body">
         <h2 class="card-title text-xl sm:text-2xl md:text-3xl">{cardTitle}</h2>
         <p class="py-2 text-base text-justify leading-6 sm:leading-8 sm:text-lg">
           {cardBodyText}
         </p>
+        <div class="card-actions justify-end">
+          <a href="/contact" class="hover:cursor-pointer">
+            <button type="button" class="btn btn-lg btn-soft hover:btn-accent">
+              Find Out More
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
--- routes/services/index.tsx
+++ routes/services/index.tsx
@@ -4,31 +4,22 @@ import CertificationBadge from "../../components/CertificationBadge.tsx";
 
 export default define.page(() => {
   return (
-    <div class="flex flex-col justify-around mx-2 md:mx-0 *:mt-8 md:flex-row xl:flex-col xl:justify-center">
+    <div class="flex flex-col justify-center mx-2 md:mx-0 *:mt-8 md:mb-6 md:flex-row xl:flex-col xl:justify-center">
       <div class="flex flex-col justify-center items-center gap-4 md:mx-4 xl:gap-8 xl:flex-row xl:items-stretch xl:h-full">
         <ServicesCard
-          imageUrl="mohammad-rahmani-oXlXu2qukGE-unsplash.jpg"
-          imageAlt="A photo of a computer monitor with lines of code on the screen."
-          imageTitle="Photo by Mohammad Rahmani on Unsplash"
           cardTitle="Software Development"
           cardBodyText="Robust, elegant design, the core of software engineering, lies at the heart of what I build. As the rate of technological advancement accelerates every day, the competitive gap between businesses that embrace and businesses that resist change widens. Software development bridges that gulf. While your business navigates these technological shifts, my work is to build a solution that ensures you stay ahead of the curve."
         />
         <ServicesCard
-          imageUrl="daria-nepriakhina-zoCDWPuiRuA-unsplash.jpg"
-          imageAlt="A photo of a whiteboard with sticky notes."
-          imageTitle="Photo by Daria Nepriakhina on Unsplash"
           cardTitle="Digital Transformation"
           cardBodyText="Bridging the digital divide is the essence of my professional work. Overhauling the analogue systems that businesses inherit seems overwhelming, but the longer they linger, the more the inefficiency accrues over time. Digital transformation provides the answer. Sometimes, finding the solution for your business is as simple as getting a fresh perspective, and that's where I facilitate the transition."
         />
         <ServicesCard
-          imageUrl="kenny-eliason-y_6rqStQBYQ-unsplash.jpg"
-          imageAlt="A middle-aged man showing a young man something on a laptop."
-          imageTitle="Photo by Kenny Eliason on Unsplash"
           cardTitle="Technical Instruction"
           cardBodyText="With over 7 years of experience as a teacher, I have helped hundreds of people of all ages bridge the knowledge gap between where they were and where they wanted to be. Learning a new tool or skill can be the key to boosting your career or obtaining that qualification, but without guidance, it can feel like an unsurmountable task. As a teacher, I can help you learn, no matter the subject area."
         />
       </div>
-      <div class="flex h-full pb-4 justify-center items-center align-middle gap-2 md:flex-col md:gap-8 xl:flex-row lg:pb-8">
+      <div class="flex h-full pb-4 justify-center items-center align-middle gap-2 md:flex-col md:gap-8 md:ml-8 xl:ml-0 xl:flex-row lg:pb-8">
         <CertificationBadge
           imageSrc="digital-futures-academy-engineer-badge.png"
           imageAlt="The Digital Futures Academy Certificate of Completion badge."
