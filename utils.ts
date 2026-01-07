c960a4a279e9bb8180cb9371bb697137993a62d0
Harry <harrycurtis@live.com>
2026-01-07 16:27:35 +0000

    created util functions for paragraph elision

--- utils.ts
+++ utils.ts
@@ -1,3 +1,32 @@
 import { createDefine } from "fresh";
 
 export const define = createDefine<{ theme: "rustic" | "night" }>();
+
+class OddParagraphError extends Error {
+  constructor() {
+    super(
+      "An odd paragraph was provided as an input; e.g. a paragraph with no full-stops before 255 characters.",
+    );
+  }
+}
+
+export const elideParagraph = (paragraph: string): string => {
+  if (paragraph.length < 256) return paragraph; // No elision required.
+  let shortenedParagraph = paragraph.slice(0, 255);
+  while (shortenedParagraph.at(shortenedParagraph.length - 1) !== ".") {
+    shortenedParagraph = shortenedParagraph.slice(
+      0,
+      shortenedParagraph.length - 1,
+    ); // Shorten until next full-stop.
+    if (shortenedParagraph.length === 1) {
+      throw new OddParagraphError();
+    } // Prevent infinite loop in edge cases.
+  }
+  return shortenedParagraph;
+};
+
+export const appendEllipsisToParagraph = (paragraph: string): string => {
+  return paragraph.at(paragraph.length - 1) === " "
+    ? paragraph + "…"
+    : paragraph + " …";
+};
