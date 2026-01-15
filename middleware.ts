eb9add7931acfd6884e729ba4275f3572c0b2083
Harry Curtis <harry@boothbooks.co.uk>
2026-01-15 13:17:44 +0000

    updated middleware design

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,26 @@
 import { getCookies } from "@std/http/cookie";
-import { define } from "./utils.ts";
-
+import { define, getPost, getPosts } from "./utils.ts";
+import { Post } from "@/types.ts";
 export const themeMiddleware = define.middleware(async (ctx) => {
   const cookies = getCookies(ctx.req.headers);
-
-  ctx.state.theme = cookies.theme || "rustic";
-
+  const theme = cookies.theme;
+  if (theme === "rustic" || theme === "night") {
+    ctx.state.theme = theme;
+  }
+  return await ctx.next();
+});
+export const addPostToContext = define.middleware(async (ctx) => {
+  const slug = ctx.params.slug;
+  const post: Post | null = await getPost(slug);
+  if (post) {
+    ctx.state.post = post;
+  }
+  return await ctx.next();
+});
+export const addPostsToContext = define.middleware(async (ctx) => {
+  const posts: Post[] = await getPosts();
+  if (posts) {
+    ctx.state.posts = posts;
+  }
   return await ctx.next();
 });
