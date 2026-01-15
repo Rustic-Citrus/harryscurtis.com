38bca89819642d5e546ca8d59279aed752344e13
Harry Curtis <harry@boothbooks.co.uk>
2026-01-15 14:51:35 +0000

    deleted redundant api routes

--- main.ts
+++ main.ts
@@ -4,9 +4,7 @@ import {
   addPostToContext,
   themeMiddleware,
 } from "./middleware.ts";
-import { ContextState, Post } from "@/types.ts";
-import { join } from "@std/path";
-import { getPost } from "@/utils.ts";
+import { ContextState } from "@/types.ts";
 import { getCookies, setCookie } from "@std/http/cookie";
 export const app = new App<ContextState>();
 app.get("/api/theme", (ctx) => {
@@ -24,36 +22,6 @@ app.get("/api/theme", (ctx) => {
   const referrer = ctx.req.headers.get("referer") || "/";
   headers.set("location", referrer);
   return new Response(null, { status: 302, headers });
-});
-app.get("/api/posts", async () => {
-  const posts: Post[] = [];
-  const postsDir = join(Deno.cwd(), "posts");
-  try {
-    for await (const entry of Deno.readDir(postsDir)) {
-      if (entry.isFile && entry.name.endsWith(".md")) {
-        const slug = entry.name.replace(".md", "");
-        const post = await getPost(slug);
-        if (post) {
-          posts.push(post);
-        }
-      }
-    }
-  } catch (error) {
-    if (!(error instanceof Deno.errors.NotFound)) {
-      throw error;
-    }
-  }
-  posts.sort((a, b) => b.date.getTime() - a.date.getTime());
-  const body = JSON.stringify(posts);
-  return new Response(body);
-});
-app.get("/api/post/:slug", addPostToContext, (ctx) => {
-  if (!ctx.state.post) {
-    return new Response("Post not found!", { status: 404 });
-  }
-  const post = ctx.state.post;
-  const body = JSON.stringify(post);
-  return new Response(body);
 });
 app.get("/", addPostsToContext, (ctx) => {
   if (ctx.state.posts) {
