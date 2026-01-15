90151add442a7527b002755790a0c5c881c66111
Harry Curtis <harry@boothbooks.co.uk>
2026-01-15 13:03:21 +0000

    updated custom types

--- types.ts
+++ types.ts
@@ -6,3 +6,9 @@ export interface Post {
   snippet: string;
   tags: string[];
 }
+export interface ContextState {
+  theme: "rustic" | "night";
+  featuredPosts?: Post[];
+  post?: Post;
+  posts?: Post[];
+}
